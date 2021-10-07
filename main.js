f0b20d3a09a52c6166098a38cf9e08562b66f890
neargle <nearg1e.com@gmail.com>
2021-10-07 20:35:05 +0800

    main function for show all existing crx

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import './style.css'
 import {check_all_crx, check_one} from './scouter/scouter'
 
-// check_all_crx(console.log)
-const id = "oeopbcgkkoapgobdbedcemjljbihmemj"
-check_one(id, console.log)
 
+const callback = (crx_info) => {
+  const element_text = `<li><b> ${crx_info.name} </b> (<a href="${crx_info.url}">${crx_info.url}</a>)</li>`
+  const app_div = document.querySelector("#app")
+  app_div.innerHTML = app_div.innerHTML + element_text;
+}
+
+check_all_crx(callback)
