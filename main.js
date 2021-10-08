4e7e1000203ac610924ac111e037a9cadf23ffc7
neargle <nearg1e.com@gmail.com>
2021-10-08 17:15:21 +0800

    show navigator plugins

--- main.js
+++ main.js
@@ -3,16 +3,26 @@ import './style.css'
 const demo = async () => {
 
     // import {check_all_crx, check_one} from './scouter/scouter'
-    const { check_all_crx } = await import('./scouter/scouter')
+    const { check_all_crx, navigator_plugins } = await import('./scouter/scouter')
     
-    
-    const callback = (crx_info) => {
+    const append_text = (crx_info, element_id) => {
       const element_text = `<li><b> ${crx_info.name} </b> (<a href="${crx_info.url}">${crx_info.url}</a>)</li>`
-      const app_div = document.querySelector("#app")
+      const app_div = document.querySelector(element_id)
       app_div.innerHTML = app_div.innerHTML + element_text;
     }
     
+    const callback = (crx_info) => {
+      append_text(crx_info, "#app")
+    }
+    
+    const nps = navigator_plugins()
+    for (let i = 0; i < nps.length; i++) {
+      const crx_info = nps[i]
+      append_text(crx_info, "#app-plugins")
+    }
+
     check_all_crx(callback)
+
 
 }
 
